6c654749f010a9550bd311bb8a1ab4f8a47abafe
dsgygb <dsgygb@126.com>
2016-01-05 02:05:50 +0800

    dsgygb

---
+++ content/second.md
@@ -0,0 +1,10 @@
++++
+Categories = ["Development", "GoLang"]
+Description = ""
+Tags = ["Development", "golang"]
+date = "2016-01-04T22:29:27+08:00"
+menu = "main"
+title = "second"
+
++++
+
---
+++ server/deploy.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+if [ $# -eq 1 ]
+  then username="$1"
+  else echo "pleace input param 1 for github username";exit 1
+fi
+
+root_dir=$(cd "$(dirname "$0")/../"; pwd)
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+repo_path=${cur_dir}/repository
+
+if [ ! -d "$repo_path" ]
+then
+mkdir "${repo_path}"
+fi
+
+cd ${repo_path}
+
+rm -rf ${repo_path}/${username}/
+git clone https://github.com/dsgygb/blog-hugo.git ${username}
+
+cp -rf ${repo_path}/${username}/content/ ${root_dir}/content/
+
+. ${root_dir}/deploy.sh
+
+
+
+
+#
+#cd repository
+#
+##echo ${username}
+#
+#git clone https://github.com/dsgygb/blog-hugo.git ${username}
+#
+#cd ${username}
+#
+#git pull
+#
+#cp -r -f ../../repository/${username}/content/ ../../../content/
+#
+#cd ../../../
+#
+#./deploy.sh
+
+
+
+
